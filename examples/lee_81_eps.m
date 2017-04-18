1e91169fa06596d25f63ed999a98cad2482ebde8
Oleksandr Gituliar <oleksandr@gituliar.net>
2017-04-18 16:17:59 +0200

    Update examples

---
+++ examples/lee_81_eps.m
@@ -0,0 +1,1 @@
+{{3*eps/(x+1)+eps/x,-1/11*eps/(x+1)+1/11*eps/x,3/22*eps/(x+1)-3/22*eps/x},{90*eps/(x+1)-20*eps/x,-54/11*eps/(x+1)+4*eps/x,-69/22*eps/(x+1)+3/2*eps/x},{-6*eps/(x+1)+70/3*eps/x,8/11*eps/(x+1)-4/3*eps/x,32/11*eps/(x+1)-4*eps/x}}
